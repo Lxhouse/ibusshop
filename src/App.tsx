2badb25656d01a47a4a506556eac941c341488a8
LxHouse <364403299@qq.com>
2024-11-04 16:30:58 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from './components/Button';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 默认按钮 */}
+      <Button>Default Button</Button>
+
+      {/* 主按钮 */}
+      <Button btnType={ButtonType.Primary}>Primary Button</Button>
+
+      {/* 危险按钮 */}
+      <Button btnType={ButtonType.Danger}>Danger Button</Button>
+
+      {/* 链接按钮 */}
+      <Button btnType={ButtonType.Link} href="https://example.com">
+        Link Button
+      </Button>
+
+      {/* 大小不同的按钮 */}
+      <Button size={ButtonSize.Large}>Large Button</Button>
+      <Button size={ButtonSize.Small}>Small Button</Button>
+
+      {/* 禁用的链接按钮 */}
+      <Button btnType={ButtonType.Link} href="https://example.com" disabled>
+        Disabled Link Button
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import classNames from 'classnames';
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm',
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link',
+}
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+type NativeButtonProps = BaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps &
+  React.AnchorHTMLAttributes<HTMLElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+
+const Button: React.FC<ButtonProps> = ({
+  btnType = ButtonType.Default,
+  disabled = false,
+  size,
+  children,
+  href,
+  className,
+  ...restProps
+}) => {
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disabled: btnType === ButtonType.Link && disabled,
+  });
+
+  return btnType === ButtonType.Link && href ? (
+    <a href={href} className={classes} {...restProps}>
+      {children}
+    </a>
+  ) : (
+    <button className={classes} disabled={disabled} {...restProps}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
